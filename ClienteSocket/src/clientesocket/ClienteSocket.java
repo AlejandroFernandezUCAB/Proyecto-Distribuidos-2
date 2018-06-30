04ce6d35695e58b89f78666f85e7850950b737d3
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2018-06-30 15:39:14 -0400

    Haciendo conexion

--- ClienteSocket/src/clientesocket/ClienteSocket.java
+++ ClienteSocket/src/clientesocket/ClienteSocket.java
@@ -5,6 +5,16 @@
  */
 package clientesocket;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author pedro
@@ -14,8 +24,20 @@ public class ClienteSocket {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        try {
+            String serverAddress = JOptionPane.showInputDialog(
+                    "Enter IP Address of a machine that is\n" +
+                            "running the date service on port 9092:");
+            Socket s = new Socket(serverAddress, 9092);
+            BufferedReader input =
+                    new BufferedReader(new InputStreamReader(s.getInputStream()));
+            String answer = input.readLine();
+            JOptionPane.showMessageDialog(null, answer);
+            System.exit(0);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteSocket.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- ServidorSocket/src/servidorsocket/ServidorSocket.java
+++ ServidorSocket/src/servidorsocket/ServidorSocket.java
@@ -5,6 +5,15 @@
  */
 package servidorsocket;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 /**
  *
  * @author pedro
@@ -14,8 +23,24 @@ public class ServidorSocket {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(9092);
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                try {
+                    PrintWriter out =
+                        new PrintWriter(socket.getOutputStream(), true);
+                    out.println(listener);
+                } finally {
+                    socket.close();
+                }
+            }
+        }
+        finally {
+            listener.close();
+        }
+
     }
     
 }
