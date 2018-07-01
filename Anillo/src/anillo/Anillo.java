955e7195e27a29b4017aab449908c721f1ec67ab
Pedro Alejandro Fernández Fernández <pafernandez.14@est.ucab.edu.ve>
2018-07-01 16:02:32 -0400

    Agregando libreria

--- Anillo/src/anillo/Anillo.java
+++ Anillo/src/anillo/Anillo.java
@@ -8,15 +8,11 @@ package anillo;
 //
 
 import com.google.gson.Gson;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
